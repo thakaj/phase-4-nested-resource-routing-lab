04dececf63013008fa37175c76f6dd914c07d5fd
thakaj <taulant.hakaj@scranton.edu>
2021-09-24 12:11:24 -0400

    commit tree b7b3534520a27008ff0ac3345ebca9e2ed88a64d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
-    render json: items, include: :user
+    end
+    render json: items, except: [:created_at, :updated_at], include: :user, except: [:created_at, :updated_at]
   end
-
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items.find(params[:id])
+    else
+      items = Item.find(params[:id])
+    end
+    render json: items, include: :user, except: [:created_at, :updated_at]
+  end
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items.create!(name: params[:name], description: params[:description], price: params[:price])
+    else
+      items = Item.create!(name: params[:name], description: params[:description], price: params[:price])
+    end
+    render json: items, include: :user, except: [:created_at, :updated_at], status: 201
+  end
+  private
+  def render_not_found
+    render json: {errors: "Not found"}, status: 404
+  end
 end
